b175b5784118961d0f731be66b15d1400f29cc78
Adesh <Adeshpokhrel15@gmail.com>
2022-05-04 20:09:49 +0545

    Home Screen

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -4,9 +4,115 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class mainScreen extends StatelessWidget {
+  Widget singleBook() {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 5,
+      ),
+      height: 240,
+      width: 150,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Expanded(flex: 2, child: Image.asset('assets/book1.jpg')),
+        Expanded(
+            child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('The Indian Girl',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                Text(' Rs.200',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey)),
+                Row(children: [
+                  Expanded(
+                    child: Container(
+                      height: 30,
+                      width: 50,
+                      color: Colors.black,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Text(
+                            '1 book',
+                            style: TextStyle(fontSize: 10),
+                          )),
+                          Center(
+                            child: Icon(
+                              Icons.arrow_drop_down,
+                              size: 20,
+                              color: Colors.red,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Expanded(
+                      child: Container(
+                    height: 30,
+                    width: 50,
+                    color: Colors.blue,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.remove,
+                          size: 15,
+                          color: Colors.red,
+                        ),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Icon(
+                          Icons.remove,
+                          size: 15,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ))
+                ])
+              ],
+            ),
+          ),
+        ))
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       drawer: Drawer(),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
@@ -53,6 +159,114 @@ class mainScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage('assets/book.jpg')),
+              ),
+              child: Row(children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(right: 130, bottom: 10),
+                          child: Container(
+                            height: 50,
+                            width: 100,
+                          ),
+                        ),
+                        Text(
+                          'There is no friend as loyal as a book',
+                          style: TextStyle(
+                              fontSize: 28,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Popular',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                    Text(
+                      'See All',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ],
+                )),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Most Popular',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                    Text(
+                      'See All',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ],
+                )),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                  singleBook(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

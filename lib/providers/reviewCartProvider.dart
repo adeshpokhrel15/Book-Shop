ecb4c7c664f2e0e078084f5f6578e5829906ee62
Adesh <Adeshpokhrel15@gmail.com>
2022-05-10 21:52:49 +0545

    cart provider

--- lib/providers/reviewCartProvider.dart
+++ lib/providers/reviewCartProvider.dart
@@ -1,17 +1,51 @@
+import 'package:bookshop/models/cartModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final cartProvider =
+    StreamProvider((ref) => FirebaseAuth.instance.authStateChanges());
+final cartStream =
+    StreamProvider.autoDispose((ref) => ReviewCartProvider().getCartData());
 
+class ReviewCartProvider {
+  CollectionReference dbCart = FirebaseFirestore.instance.collection('users');
 
-// import 'package:bookshop/models/reviewCartItem.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+  Future<dynamic> addToCart({
+    required String cartId,
+    required String cartImage,
+    required int cartPrice,
+    required int cartQuantity,
+  }) async {
+    try {
+      final response =
+          await FirebaseFirestore.instance.collection('users').add({
+        'cartId': cartId,
+        'cartImage': cartImage,
+        'cartPrice': cartPrice,
+        'cartQuantity': cartQuantity,
+      });
+      return 'success';
+    } on FirebaseException catch (e) {
+      return '';
+    }
+  }
 
-// class CartProvider  extends StateNotifier<List<ReviewCartModel>> {
-//   CartProvider() : super([]);
+  Stream<List<CartModel>> getCartData() {
+    final data = dbCart.snapshots().map((event) => _getFromSnap(event));
 
+    return data;
+  }
 
-//   String addProduct(ReviewCartModel reviewCartModel) {
-    
-
-
-
-
-// }
+  List<CartModel> _getFromSnap(QuerySnapshot querySnapshot) {
+    return querySnapshot.docs.map((e) {
+      final data = e.data() as Map<String, dynamic>;
+      return CartModel(
+        cartId: data['cartId'],
+        cartImage: data['cartImage'],
+        cartPrice: data['cartPrice'],
+        cartQuantity: data['cartQuantity'],
+      );
+    }).toList();
+  }
+}
